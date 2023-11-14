a78fce77c341ed565615e8f52c88271d08e5b516
jhyuk <jh.jang@outlook.com>
2023-11-14 17:23:17 +0900

    to-do-list-dr

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/main';
+import Detail from './pages/detail';
+
 const App = () => {
-  return <div className='bg-red-100'>Hellow, React!!!!!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/:id' element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
+
 export default App;
